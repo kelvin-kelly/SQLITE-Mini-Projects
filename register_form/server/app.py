7367497a5dea48a35c5948a4610c1cffb71b9aa0
Kelvin Machira <machirakelvin81@gmail.com>
2024-07-09 11:07:16 +0300

    offline testing

--- register_form/server/app.py
+++ register_form/server/app.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 # Configure the database URI
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///register.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/kelly/Desktop/FLASK/SQLITE/register_form/instance/register.db'
 
 db = SQLAlchemy(app)
 
@@ -16,11 +16,7 @@ class Register(db.Model):
     fname = db.Column(db.String(40))
     lname = db.Column(db.String(40))
     pet = db.Column(db.String(40))
-    
-    def _init_(self, fname, lname, pet):
-        self.fname = fname
-        self.lname = lname
-        self.pet = pet
+
 
 @app.route('/')
 def index():
@@ -28,25 +24,23 @@ def index():
 
 @app.route('/submit', methods=['GET', 'POST'])
 def submit():
+    
     if request.method == 'POST':
         fname = request.form['fname']
         lname = request.form['lname']
         pet = request.form['pet']
         
-        reg = Register(fname, lname, pet)
+        reg = Register(fname=fname, lname=lname, pet=pet)
         db.session.add(reg)
         db.session.commit()
-    elif request.method == 'GET':
-        regResult = db.session.query(Register).filter(Register.id == 1).first()
-        if regResult:
-            print(regResult.fname)
-        else:
-            print("No result found.")
         
-    return render_template('success.html', data=fname)
-    return render_template('index.html') 
+        regResult = db.session.query(Register).filter(Register.id ==1)
+        for result in regResult :
+            print(result.fname)
+            
+    return render_template('success.html', data = fname)
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all() 
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
