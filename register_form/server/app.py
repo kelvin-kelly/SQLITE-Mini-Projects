0fa6ee26b428ec680e97b18099df46233aa2f304
Kelvin Machira <machirakelvin81@gmail.com>
2024-07-09 11:24:21 +0300

    getting path with os

--- register_form/server/app.py
+++ register_form/server/app.py
@@ -1,8 +1,14 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+import os
+
 
 # supa_pass = 'SUPABBASEsupabase'
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_URI  = 'sqlite:///'+ os.path.join(BASE_DIR, 'register.db') 
 
+
+print(BASE_DIR)
 app = Flask(__name__)
 
 # Configure the database URI
