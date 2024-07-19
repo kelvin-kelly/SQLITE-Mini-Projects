b8a5d31cef963dd21e3cc25897e88a9097e3a195
Kelvin Machira <machirakelvin81@gmail.com>
2024-07-19 12:45:23 +0300

    custom error templates and routes

--- BLOGGER/app.py
+++ BLOGGER/app.py
@@ -8,9 +8,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/user/<name>')
-def user(name):
-    return f'<h1>Hello {name}</h1>'
+@app.route('/user/')
+def user():
+     return render_template('user.html',)
+
+# custom error pages
+# invalid url
+@app.errorhandler(404)
+def page_not_found(e):
+     return render_template('404.html'), 404
+
+# Internal server error
+@app.errorhandler(500)
+def page_not_found(e):
+     return render_template('500.html'), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
