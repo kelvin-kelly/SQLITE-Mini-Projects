06c92e2b8bbf65437e62aced7cf74b5501373453
Kelvin Machira <machirakelvin81@gmail.com>
2024-07-22 14:29:21 +0300

    Flask WTForm

--- BLOGGER/app.py
+++ BLOGGER/app.py
@@ -1,8 +1,16 @@
 from flask import Flask, render_template, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'MY_SECRET_KET'
 
+# form class
+class NamerForm(FlaskForm):
+     name = StringField("What's Your Name", validators=[DataRequired()])
+     submit = SubmitField("Submit")
 # route decorators
 @app.route('/')
 def index():
@@ -11,6 +19,17 @@ def index():
 @app.route('/user/')
 def user():
      return render_template('user.html'), 200
+
+@app.route('/name', methods=['GET','POST'])
+def name():
+     name = None
+     form = NamerForm()
+     # validation
+     if form.validate_on_submit():
+          name = form.name.data
+          form.name.data = ''
+          
+     return render_template('name.html', name=name, form=form)
 
 # custom error pages
 # invalid url
@@ -22,6 +41,7 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
      return render_template('500.html'), 500
+
 
 
 if __name__ == '__main__':
